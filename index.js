cd29b31a03af06fd2b5136051632a26223dd727a
crstllracing231 <crstllracing231.alt@gmail.com>
2025-12-06 20:52:18 +0800

    complete homepage

---
+++ index.js
@@ -0,0 +1,26 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) header.classList.add('scrolled');
+    else header.classList.remove('scrolled');
+});
+
+function checkLoginStatus() {
+    const isLoggedIn = true;
+    return isLoggedIn;
+}
+
+function updateHeader() {
+    const loggedOut = document.getElementById('logged-out');
+    const loggedIn = document.getElementById('logged-in');
+
+    if (checkLoginStatus()) {
+        if (loggedOut) {loggedOut.style.display = 'none';}
+        if (loggedIn) {loggedIn.style.display = 'flex';}
+    }
+    else {
+        if (loggedIn) {loggedIn.style.display = 'none';}
+        if (loggedOut) {loggedOut.style.display = 'flex';}
+    }
+}
+
+document.addEventListener('DOMContentLoaded', updateHeader);
